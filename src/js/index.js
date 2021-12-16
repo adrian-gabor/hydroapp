2d5972d8322b15b4a477b53c2efa702d4e11f1e8
Adrian Gabor <adgabor97@gmail.com>
2021-12-17 00:30:30 +0100

    version beta

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,33 @@
 import '../scss/main.scss';
+import { registerSW } from './pwa.js';
+registerSW();
 
-// uncomment the lines below to enable PWA
-// import {registerSW} from './pwa.js';
-// registerSW();
 
-/* place your code below */
+const number = document.querySelector('.number--js');
+const add = document.querySelector('.add--js');
+const sub = document.querySelector('.sub--js');
+const key = new Date().toISOString().slice(0,10)
+let glass;
+if (localStorage.getItem(`${key}`)) {
+    glass = localStorage.getItem(`${key}`)
+    number.innerHTML = glass
+} else {
+    glass = 0
+    number.innerHTML = glass;
+}
 
-console.log('HELLO 🚀')
+add.addEventListener('click', (e) => {
+    e.preventDefault();
+    glass = glass + 1
+    localStorage.setItem(key, glass)
+    number.innerHTML = localStorage.getItem(key)
+})
+
+sub.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (glass > 0) {
+        glass = glass - 1
+        localStorage.setItem(key, glass)
+        number.innerHTML = localStorage.getItem(key)
+    }
+})
